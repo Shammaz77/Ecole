af6fb9dc821251cf0bb321e6f899b2c278fb03dc
shammaz77 <shammasnhd@gmail.com>
2024-06-14 05:06:13 +0530

    initial commit!

---
+++ js/main.js
@@ -0,0 +1,49 @@
+AOS.init({
+    duration: 1000,
+});
+
+
+
+
+
+
+
+
+
+
+const numberElements = document.querySelectorAll('.number');
+
+function updateNumbers() {
+  const targetNumbers = [130, 40, 2]; // Target numbers for each h1 element
+
+  for (let i = 0; i < numberElements.length; i++) {
+    const element = numberElements[i];
+    const targetNumber = targetNumbers[i];
+
+    // Increment the current text content towards the target number
+    let currentNumber = parseInt(element.textContent, 10);
+    if (currentNumber < targetNumber) {
+      currentNumber++;
+    } else if (currentNumber > targetNumber) {
+      currentNumber--;
+    }
+
+    // Update text content with plus sign after the number
+    element.textContent = `${currentNumber} +`;
+  }
+
+  // Update again after a short delay for animation effect
+  setTimeout(updateNumbers, 50); // Adjust delay (in milliseconds) as needed
+}
+
+updateNumbers();
+
+
+
+
+
+
+
+
+
+
